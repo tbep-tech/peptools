a5631cd496e8c217a51d428ae1c68a5c6cd565ed
fawda123 <mbafs2012@gmail.com>
2020-03-26 17:11:21 -0400

    added peptargets

---
+++ R/peptargets.R
@@ -0,0 +1,18 @@
+#' Bay segment targets
+#'
+#' Bay segment targets 
+#' 
+#' @format A \code{data.frame} object
+#' @family utilities
+#' @examples
+#' \dontrun{
+#' peptargets <- tibble::tibble(
+#'   bay_segment = factor(c('Western', 'Central', 'Eastern'), levels = c('Western', 'Central', 'Eastern')), 
+#'   name = factor(c('Western', 'Central', 'Eastern'), levels = c('Western', 'Central', 'Eastern')), 
+#'   sd_thresh = c(6.5, 6.5, 6.5),
+#'   chla_thresh = c(5.5, 5.5, 5.5)
+#' )
+#' save(peptargets, file = 'data/peptargets.RData', compress= 'xz')
+#' }
+#' @family utilities
+"peptargets"
