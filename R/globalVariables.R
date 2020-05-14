d1d59db5aa2a700651af3d40b612aed96f0bc832
fawda123 <mbafs2012@gmail.com>
2020-05-14 07:46:25 -0400

    fixing code mismatch for build

--- R/globalVariables.R
+++ R/globalVariables.R
@@ -1,9 +1,9 @@
 globalVariables(c('BayStation', 'Chlorophyll A - Total', 'Date', 'Secchi', 'bay_segment', 'name', 'stations', 'value',
                   'chla', 'data', 'est', 'lwr.ci', 'mags', 'mdv', 'medv', 'mo', 'outcome',
                   'pepstations', 'peptargets', 'thresh', 'upr.ci', 'val', 'var', 'yr', '.', 'Action', 'chla_sd', 'outcometxt',
-                  '0.95LCL', '0.95UCL', 'status', 'tosep', 'Result', 'durats', 'segval'))
+                  '0.95LCL', '0.95UCL', 'status', 'tosep', 'Result', 'durats', 'segval', 'cexs', 'val2'))
 
-#' @importFrom stats median na.omit sd
+#' @importFrom stats median na.omit quantile sd
 NULL
 
 #' @importFrom grDevices colorRampPalette
