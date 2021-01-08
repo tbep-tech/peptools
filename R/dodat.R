0fb47416ab3913ae0cd4c5d04f3fb9b79f0fc424
fawda123 <mbafs2012@gmail.com>
2021-01-08 15:10:19 -0500

    added dodat data object, #5

---
+++ R/dodat.R
@@ -0,0 +1,22 @@
+#' Dissolved oxygen data for USGS stations
+#'
+#' Dissolved oxygen data for USGS stations
+#'
+#' @format A \code{data.frame} object
+#' @family utilities
+#' @examples 
+#' \dontrun{
+#' # get DO data from USGS stations
+#' # 01304562 is Peconic River, 01304200 is Orient Harbor
+#' # downloaded data are in UTC
+#' # for cd column, codes are often “A” (approved) or “P” (provisional)
+#' # from vignette https://cran.r-project.org/web/packages/dataRetrieval/vignettes/dataRetrieval.html
+#' # takes a few minutes to dl
+#' dodat <- readNWISuv(siteNumbers = '01304562', parameterCd = '00300') %>% 
+#'   select(site = site_no, DateTime = dateTime, do_mgl = X_00300_00000) %>%
+#'   mutate(DateTime = with_tz(DateTime, tzone = 'America/Jamaica'))
+#' 
+#' save(dodat, file = 'data/dodat.RData', compress = 'xz')
+#'
+#'}
+"dodat"
