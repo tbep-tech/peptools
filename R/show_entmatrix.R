100f92e49f27000b73310347ae65a112c76e834c
fawda123 <mbafs2012@gmail.com>
2021-02-04 18:54:23 -0500

    minor tweaks to show_entmatrix

--- R/show_entmatrix.R
+++ R/show_entmatrix.R
@@ -18,7 +18,7 @@
 #'
 #' @examples
 #' show_entmatrix(entdat)
-show_entmatrix <- function(entdat, txtsz = 3, thr = 104, cats = c(0, 1, 2), yrrng = c(2010, 2020), nrows = 10, family = NA){
+show_entmatrix <- function(entdat, txtsz = 2, thr = 104, cats = c(0, 1, 2), yrrng = c(2010, 2020), nrows = 10, family = NA){
 
   # process data to plot
   entpep <- anlz_entpep(entdat, thr = thr)
@@ -36,7 +36,7 @@ show_entmatrix <- function(entdat, txtsz = 3, thr = 104, cats = c(0, 1, 2), yrrn
     ggplot2::geom_tile(colour = 'black') +
     ggplot2::scale_y_discrete(expand = c(0, 0)) +
     ggplot2::scale_x_discrete(expand = c(0, 0), position = 'top') +
-    ggplot2::scale_fill_gradient('Prop. of samples\nexceeding', low = 'white', high = 'blue', limits = c(0, 1)) +
+    ggplot2::scale_fill_gradient('Prop. of samples', low = 'white', high = 'blue', limits = c(0, 1)) +
     ggplot2::theme_bw(base_family = family) +
     ggplot2::theme(
       axis.title = ggplot2::element_blank(),
