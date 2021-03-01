fb854aa26d9e02229b275358c73c9caa64e60338
fawda123 <mbafs2012@gmail.com>
2021-03-01 09:54:45 -0500

    beaches data object added

--- R/anlz_entpep.R
+++ R/anlz_entpep.R
@@ -17,16 +17,6 @@
 #' anlz_entpep(entdat)
 anlz_entpep <- function(entdat, thr = 104){
 
-  # beaches <- c("Alberts Landing Beach", "Camp Blue Bay Beach", "Camp Quinipet Beach", 
-  #   "Clearwater Beach", "Cornell Cooperative Extension Marine Center Beach", 
-  #   "Crescent Beach - Shelter Island", "Culloden Shores Beach", "Devon Yacht Club Beach", 
-  #   "East Lake Drive Beach", "Fifth Street Park Beach", "Fleets Neck Beach", 
-  #   "Foster Memorial Beach", "Founders Landing Beach", "Goose Creek Beach", 
-  #   "Havens Beach", "Maidstone Beach", "Meschutt Beach", "Nassau Point Causeway Beach", 
-  #   "New Suffolk Beach", "Norman E. Klipp Park Beach", "Perlman Music Camp Beach", 
-  #   "Pridwin Hotel Beach", "Shelter Island Heights Beach Club Beach", 
-  #   "Silver Sands Motel Beach", "South Jamesport Beach", "Southampton Peconic Beach & Tennis Club Beach", 
-  #   "Veteran's Memorial Park Beach", "Wades Beach")
   beaches <- c("Alberts Landing Beach", "Camp Baiting Hollow Beach (Lake)", "Camp Blue Bay Beach", "Camp Quinipet Beach", "Clearwater Beach",
       "Cornell Cooperative Extension Marine Center Beach", "Crescent Beach - Suffolk", "Culloden Shores Beach", "Devon Yacht Club Beach",
       "East Lake Drive Beach", "Emma Rose Elliston Memorial Park Beach", "Fifth Street Park Beach", "Fleets Neck Beach", "Foster Memorial Beach",
---
+++ R/beaches.R
@@ -0,0 +1,21 @@
+#' Bathing beaches
+#'
+#' Bathing beaches
+#'
+#' @format A \code{data.frame} object
+#' @family utilities
+#' @examples 
+#' \dontrun{
+#'
+#' library(sf)
+#' library(dplyr)
+#' 
+#' beaches <- read.csv('inst/extdata/beaches.csv') %>% 
+#'   st_as_sf(coords = c('Longitude', 'Latitude'), crs = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
+#'
+#' names(beaches)[1] <- 'Name'
+#' 
+#' save(beaches, file = 'data/beaches.RData')
+#'
+#' }
+"beaches"
