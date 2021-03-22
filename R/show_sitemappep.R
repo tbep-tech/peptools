d8249bc4fbfbb6998a648195dc54200e56ead76d
fawda123 <mbafs2012@gmail.com>
2021-03-22 08:36:38 -0400

    month option added to show_sitemappep, fix #8

--- R/show_sitemappep.R
+++ R/show_sitemappep.R
@@ -3,13 +3,17 @@
 #' Map water quality data for a selected year
 #'
 #' @param dat data frame of data returned by \code{\link{read_pepwq}}
-#' @param yrsel numeric for year to plot
+#' @param yrsel numeric for years to plot, see details
+#' @param mosel numeric for months to plot, see details
 #' @param param chr string indicating which water quality value to plot, one of "chla" for chlorophyll and "sd" for secchi depth
 #' @param bay_segment chr string for the bay segment, one or all of "Western", "Central", or "Eastern"
 #' @param maxrel numeric for the maximum quantile value for scaling if \code{relative = T}, this prevents outliers from skewing the scale
 #' @param relative logical indicating if sizes and colors are relative to the entire water quality data base, otherwise scaling is relative only for the points on the map
 #' 
-#' @details Year estimates for the selected parameter are based on median observations across months.  The color ramp is reversed for Secchi observations.
+#' @details 
+#' Year estimates for the selected parameter are based on median observations across months. All twelve months are used if \code{mosel = NULL} (default). Monthly estimates for the selected parameter are based on median observations across years.  All years are used if \code{yrsel = NULL} (default).
+#' 
+#' The color ramp is reversed for Secchi observations.
 #' 
 #' @family visualize
 #'
@@ -18,15 +22,42 @@
 #' @export
 #'
 #' @examples
+#' 
+#' # 2018, all months
 #' show_sitemappep(rawdat, yrsel = 2018)
-show_sitemappep <- function(dat, yrsel, param = c('chla', 'sd'), bay_segment = c('Western', 'Central', 'Eastern'), maxrel = 0.99, relative = FALSE){
+#' 
+#' # 2018, July only
+#' show_sitemappep(rawdat, yrsel = 2018, mosel = 7)
+#' 
+#' # July only, all years
+#' show_sitemappep(rawdat, mosel = 7)
+show_sitemappep <- function(dat, yrsel = NULL, mosel = NULL, param = c('chla', 'sd'), bay_segment = c('Western', 'Central', 'Eastern'), maxrel = 0.99, relative = FALSE){
 
-  # sanity check
-  if(!yrsel %in% dat$yr)
-    stop(yrsel, ' not in year range for input data')
-  
   param <- match.arg(param)
   
+  # mosel full range if null
+  if(!is.null(mosel)){
+    if(!any(mosel %in% seq(1, 12)))
+      stop('mosel must be from 1 to 12')
+    if(!all(abs(diff(mosel)) == 1))
+      stop('mosel must be in a sequence')
+  }
+  
+  if(is.null(mosel))
+    mosel <- seq(1, 12)
+
+  # full range yrsel if null
+  yrrng <- range(dat$yr, na.rm = TRUE)
+  if(!is.null(yrsel)){
+    if(!any(yrsel %in% seq(yrrng[1], yrrng[2])))
+      stop(yrsel, ' not in range ', paste(yrrng, collapse = ' to '))
+    if(!all(abs(diff(yrsel)) == 1))
+      stop('yrsel must be in a sequence')
+  }
+  
+  if(is.null(yrsel))
+    yrsel <- seq(yrrng[1], yrrng[2])
+
   mptyps <- c("CartoDB.Positron", "CartoDB.DarkMatter", "OpenStreetMap", "Esri.WorldImagery", "OpenTopoMap")
   prj <- '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'
   
@@ -35,11 +66,12 @@ show_sitemappep <- function(dat, yrsel, param = c('chla', 'sd'), bay_segment = c
     dplyr::filter(name %in% !!param) %>% 
     dplyr::filter(bay_segment %in% !!bay_segment) %>% 
     dplyr::filter(yr %in% !!yrsel) %>% 
-    dplyr::select(BayStation, bay_segment, yr, val = value, status) 
+    dplyr::filter(mo %in% !!mosel) %>% 
+    dplyr::select(BayStation, bay_segment, yr, mo, val = value, status) 
   
   if(param == 'chla')
     locs <- locs %>% 
-      dplyr::group_by(BayStation, yr) %>% 
+      dplyr::group_by(BayStation) %>% 
       dplyr::summarise(val = median(val, na.rm = TRUE)) %>% 
       dplyr::ungroup()
   
@@ -51,20 +83,18 @@ show_sitemappep <- function(dat, yrsel, param = c('chla', 'sd'), bay_segment = c
           T ~ 1
         )
       ) %>% 
-      survival::survfit(survival::Surv(val, status) ~ BayStation + yr, data = .) %>% 
+      dplyr::filter(yr %in% yrsel & mo %in% mosel) %>% 
+      survival::survfit(survival::Surv(val, status) ~ BayStation, data = .) %>% 
       summary() %>% 
       .$table %>% 
       as.data.frame %>% 
+      tibble::rownames_to_column(var = 'tosep') %>%   
       dplyr::select(
+        BayStation = tosep,
         val = median
-      ) %>% 
-      tibble::rownames_to_column(var = 'tosep') %>% 
-      dplyr::mutate(tosep = gsub('BayStation=|yr=', '', tosep)) %>% 
-      tidyr::separate(tosep, into = c('BayStation', 'yr'), sep = ', ') %>% 
-      dplyr::mutate(
-        yr = as.numeric(yr)
-      ) 
-
+        ) %>% 
+      dplyr::mutate(BayStation = gsub('BayStation=', '', BayStation))
+  
   # get max/min values for scaling, uses whole dataset if relative is true
   relvls <- range(locs$val, na.rm = TRUE)
   if(relative){
@@ -97,11 +127,23 @@ show_sitemappep <- function(dat, yrsel, param = c('chla', 'sd'), bay_segment = c
     cols <- rev(cols)
   colfun <- leaflet::colorNumeric(palette = cols, domain = relvls)
 
+  ##
   # legend label
+  
   lbs <- c('chla', 'sd')
   names(lbs) <- c('chl-a (ug/L)', 'secchi (ft)')
   nms <- names(lbs)[lbs == param]
-  leglab <- paste0(yrsel, ' ', nms)
+  
+  if(length(yrsel) > 1)
+    yrlab <- paste(range(yrsel), collapse = '-')
+  if(length(yrsel) == 1)
+    yrlab <- yrsel
+  if(length(mosel) > 1)
+    molab <- paste(lubridate::month(range(mosel), label = TRUE), collapse = '-')
+  if(length(mosel) == 1)
+    molab <- lubridate::month(mosel, label = TRUE)
+  
+  leglab <- paste0('Median ', nms, '</br>', yrlab, ', ', molab)
   
   # hover point labels
   labs <- paste('Bay station ', locs$BayStation, ', ', nms, ' ', round(locs$val, 2))
